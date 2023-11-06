db1fd8864e0d51bb82bfed8b0533f02bf5012dca
Hélène Emereau <helene.emereau@gmail.com>
2023-11-06 21:08:21 +0100

    version 9

--- script.js
+++ script.js
@@ -54,9 +54,11 @@ const motADecouvrir = [
 //Changer la couleur des boutons "Recommencer" et "Proposer un mot" au passage de la souris
 recommencer.addEventListener("mouseover", () => {
   recommencer.style.backgroundColor = "#E54500";
+  
 });
 recommencer.addEventListener("mouseout", () => {
   recommencer.style.backgroundColor = "rgb(121, 61, 29)";
+ 
 });
 
 proposer.addEventListener("mouseover", () => {
